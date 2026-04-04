a7af0184f7aefbd9817cbf038ad5a106e22299f0
Keshav <keshav9899828464@gmail.com>
2026-04-04 12:06:12 +0530

    update script

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let notes = JSON.parse(localStorage.getItem("notes")) || []
+let notes = JSON.parse(localStorage.getItem("notes")) || [];
 
-const addBtn = document.querySelector('#addBtn')
-const titleInput = document.querySelector('#titleNote')
-const contentInput = document.querySelector('#contentNote')
-const notesList = document.querySelector('#notesList')
+const addBtn = document.querySelector('#addBtn');
+const titleInput = document.querySelector('#titleNote');
+const contentInput = document.querySelector('#contentNote');
+const notesList = document.querySelector('#notesList');
 
-renderNotes()
+renderNotes();
 
 addBtn.addEventListener('click', () => {
-    const title = titleInput.value.trim()
-    const content = contentInput.value.trim()
+    const title = titleInput.value.trim();
+    const content = contentInput.value.trim();
 
     if (title === "" || content === "") return;
 
@@ -19,23 +19,25 @@ addBtn.addEventListener('click', () => {
         content
     });
 
-    localStorage.setItem('notes', JSON.stringify(notes))
-    titleInput.value = ""
-    contentInput.value = ""
+    localStorage.setItem('notes', JSON.stringify(notes));
+
+    titleInput.value = "";
+    contentInput.value = "";
 
     renderNotes();
 });
 
 function renderNotes() {
-    notesList.innerHTML = ""
+    notesList.innerHTML = "";
 
     notes.forEach(note => {
-        const li = document.createElement("li")
-        li.innerHTML =
-            `<div class="note-title">${note.title}</div>
-        <div>${note.content}</div>`
+        const li = document.createElement("li");
 
-        notesList.appendChild(li)
-    })
+        li.innerHTML = `
+            <div class="note-title">${note.title}</div>
+            <div>${note.content}</div>
+        `;
 
+        notesList.appendChild(li);
+    });
 }
