c3414f5a345c56f6f0faf580d96170b894536c48
Keshav <keshav9899828464@gmail.com>
2026-04-04 11:46:30 +0530

    notes creation

---
+++ script.js
@@ -0,0 +1,39 @@
+let notes = JSON.parse(localStorage.getItem("notes")) || []
+renderNotes()
+
+const addBtn = document.querySelector('#addBtn')
+const titleInput = document.querySelector('#titleNote')
+const contentInput = document.querySelector('#contentNote')
+
+addBtn.addEventListener('click',() => {
+    const title = titleInput.value.trim()
+    const content = contentInput.value.trim()
+
+    if(title === "" || content === "") return;
+
+    notes.push({
+        id: Date.now(),
+        title,
+        content
+    });
+
+    localStorage.setItem('notes',JSON.stringify(notes))
+    titleInput.value = ""
+    contentInput.value = ""
+
+    renderNotes();
+});
+
+function renderNotes(){
+    notesList.innerHTML = ""
+
+    notes.forEach(note => {
+        const li = document.createElement(li)
+        li.innerHTML = 
+        `<div class="note-title>${note.title}</div>
+        <div>${note.content}</div>`
+
+        notesList.appendChild(li)
+    })
+
+}
